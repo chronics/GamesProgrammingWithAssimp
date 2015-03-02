b44ab844bc0998fe4f078d1326d17e545f2ca832
Christopher James Ingram <cj-ingram@hotmail.com>
2015-03-02 13:46:55 +0000

    added DLLs to debug file

---
+++ Basic2dBlockGame/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include <SDL2\SDL.h>
+
+int main(int argc, char* args[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Quit();
+
+	return 0;
+}
